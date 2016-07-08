127bffac6100b4a051730f2c5fd17c6da36fd2b1
wenjinggu <598228662@qq.com>
2016-07-08 20:51:15 +0800

    添加注释说明

--- Publish-Subscribe.js
+++ Publish-Subscribe.js
@@ -4,6 +4,7 @@ var pubsub = {};
 (function (q) {
 	var topics = {};
 	var subUid = -1;
+	//发布或广播事件，包含特定的topic名称和参数
 	q.publish  = function (topic,args) {
 		if (!topics[topic]) {
 			return false;
@@ -15,6 +16,7 @@ var pubsub = {};
 		}
 		return this;
 	};
+	//通过特定的名称和回调函数订阅时间，topic/event触发时执行事件
 	q.subscribe = function (topic,func) {
 		if (!topics[topic]) {
 			topics[topic] = [];
@@ -26,6 +28,7 @@ var pubsub = {};
 		});
 		return token;
 	};
+	//基于订阅上的标记引用，通过特定topic取消订阅
 	q.unsubscribe = function (token) {
 		for(var m in topics){
 			if (topics[m]) {
@@ -41,9 +44,11 @@ var pubsub = {};
 		return this;
 	};
 })(pubsub);
-
+//消息记录器记录所有通过订阅者接收到的主题和数据
 var messageLogger = function (topics,data) {
 	console.log("Logging:" + topics +":" + data);
 };
+//订阅者监听订阅的topic，一旦该topic广播一个通知，就调用回调函数
 var subscription = pubsub.subscribe("vicky",messageLogger);
+//发布者发布topic
 pubsub.publish("vicky","Come on!");
