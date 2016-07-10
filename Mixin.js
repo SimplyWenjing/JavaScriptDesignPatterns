a68865f32e4c9abc9be7cc70002fdceaf62d4904
wenjinggu <598228662@qq.com>
2016-07-10 13:11:33 +0800

    Mixin

---
+++ Mixin.js
@@ -0,0 +1,35 @@
+var Car = function (settings) {
+	this.model = settings.model;
+	this.color = settings.color; 
+};
+
+//Mixin
+var Mixin = function () {};
+Mixin.prototype = {
+	driveForward: function () {
+		console.log("drive forward");
+	},
+	driveBackward: function () {
+		console.log("drive backward");
+	}
+};
+
+//通过一个方法将现有对象扩展到另一个对象上
+function extend (receivingClass,givingClass) {
+	for (var methodName in givingClass.prototype){
+		if(! receivingClass.prototype[methodName]){
+			receivingClass.prototype[methodName] = givingClass.prototype[methodName];
+		}
+	}
+}
+//使用
+extend(Car,Mixin);
+
+//创建一个Car
+var myCar = new Car( {
+	model: "Ford",
+	color: "red"
+});
+
+myCar.driveForward();//drive forward
+
